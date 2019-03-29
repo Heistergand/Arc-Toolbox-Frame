d4e80b1ca5ff80e143a91e1b0d5229c9c8307ddf
Heistergand <16416532+Heistergand@users.noreply.github.com>
2019-03-29 20:10:29 +0100

    Create arc-toolbox-frame.meta.js

---
+++ arc-toolbox-frame.meta.js
@@ -0,0 +1,18 @@
+    
+// ==UserScript==
+// @id             iitc-plugin-arc-toolbox-frame
+// @name           IITC plugin: Arc Toolbox Frame
+// @category       Layer
+// @version      0.2
+// @description  Combines Arc menu buttons in a frame.
+// @author       YET ANOTHER ENLIGHTENED AGENT
+// @include        *://*.ingress.com/intel*
+// @include        *://*.ingress.com/mission/*
+// @match          *://*.ingress.com/intel*
+// @match          *://*.ingress.com/mission/*
+// @grant        none
+
+// @downloadURL https://github.com/Heistergand/arc-toolbox-frame/raw/master/arc-toolbox-frame.user.js
+
+// @updateURL https://github.com/Heistergand/arc-toolbox-frame/raw/master/arc-toolbox-frame.meta.js
+// ==/UserScript==
