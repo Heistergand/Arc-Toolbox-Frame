cac1c6de955e09127aac88837c2b824d318e5ab9
Heistergand <info@heistergand.com>
2023-08-03 23:52:55 +0200

    Update arc-toolbox-frame.user.js

--- arc-toolbox-frame.user.js
+++ arc-toolbox-frame.user.js
@@ -2,17 +2,12 @@
 // @id             iitc-plugin-arc-toolbox-frame
 // @name           IITC plugin: Arc Toolbox Frame
 // @category       Layer
-// @version      0.2
+// @version      1.0
 // @description  Combines Arc menu buttons in a frame.
 // @author       YET ANOTHER ENLIGHTENED AGENT
-// @include        *://*.ingress.com/intel*
-// @include        *://*.ingress.com/mission/*
-// @match          *://*.ingress.com/intel*
-// @match          *://*.ingress.com/mission/*
+// @match          https://intel.ingress.com/*
 // @grant        none
-
 // @downloadURL https://github.com/Heistergand/arc-toolbox-frame/raw/master/arc-toolbox-frame.user.js
-
 // @updateURL https://github.com/Heistergand/arc-toolbox-frame/raw/master/arc-toolbox-frame.meta.js
 // ==/UserScript==
 
