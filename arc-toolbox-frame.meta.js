0428b85cac68d67d38a20da9edccdd72939ac9fd
Heistergand <info@heistergand.com>
2024-04-28 23:28:47 +0200

    Update arc-toolbox-frame.meta.js

--- arc-toolbox-frame.meta.js
+++ arc-toolbox-frame.meta.js
@@ -2,11 +2,11 @@
 // @id             iitc-plugin-arc-toolbox-frame
 // @name           IITC plugin: Arc Toolbox Frame
 // @category       Layer
-// @version      1.1
-// @description  Combines Arc menu buttons in a frame.
-// @author       YET ANOTHER ENLIGHTENED AGENT
+// @version        1.2
+// @description    Combines Arc menu buttons in a frame.
+// @author         Heistergand
 // @match          https://intel.ingress.com/*
-// @grant        none
-// @downloadURL https://github.com/Heistergand/arc-toolbox-frame/raw/master/arc-toolbox-frame.user.js
-// @updateURL https://github.com/Heistergand/arc-toolbox-frame/raw/master/arc-toolbox-frame.meta.js
+// @grant          none
+// @downloadURL    https://github.com/Heistergand/arc-toolbox-frame/raw/master/arc-toolbox-frame.user.js
+// @updateURL      https://github.com/Heistergand/arc-toolbox-frame/raw/master/arc-toolbox-frame.meta.js
 // ==/UserScript==
